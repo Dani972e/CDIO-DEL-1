6056411754fbf25c1a7a62cbce9cb95dc3fa9d54
Daniel Anusic <anusic@live.dk>
2016-10-03 10:09:27 +0200

    Update Player.java

--- 40_del1_nyt/src/spil/Player.java
+++ 40_del1_nyt/src/spil/Player.java
@@ -8,20 +8,46 @@ package spil;
 
 public class Player {
 
+	/**
+	 * Indkapslede klasse variabler, "fields."
+	 *
+	 * @param MAX_SCORE  maksimale score for en spiller
+	 * @param id spiller id
+	 * @param name spillernavn
+	 * @param score spillerens score
+	 */
 	private final int MAX_SCORE = 40;
 	private int id;
 	private String name;
 	private int score;
 
+	/**
+	 * constructor med to parameter int id og String name.
+	 *
+	 * @param id spiller id
+	 * @param name spillernavn
+	 */
 	public Player(int id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 
+	/**
+	 * metode der ligger amount til score.
+	 *
+	 * @param amount er den der bliver lagt til spiller score.
+	 */
 	public void addToScore(int amount) {
 		score += amount;
 	}
 
+	/**
+	 * boolean metode
+	 * hvis score er større end lig med 40, har spilleren vundet.
+	 * 
+	 * @return true
+	 * @return false
+	 */
 	public boolean hasWon() {
 		if (score >= MAX_SCORE) {
 			System.out.println("\n[" + id + "] " + name + " has won! Congratulations!\n");
@@ -31,14 +57,29 @@ public class Player {
 		}
 	}
 
+	/**
+	 * gettermetode for id
+	 * 
+	 * @return id spiller id
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * gettermetode for name
+	 * 
+	 * @return name spiller navn
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * getter metode for score
+	 * 
+	 * @return score spillerens score
+	 */
 	public int getScore() {
 		return score;
 	}
