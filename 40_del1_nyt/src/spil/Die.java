82c42ff3b30df7227b670a8992caa8fb830cdbd1
Daniel Larsen <iyyel@users.noreply.github.com>
2016-10-06 11:07:16 +0200

    Update Die.java

--- 40_del1_nyt/src/spil/Die.java
+++ 40_del1_nyt/src/spil/Die.java
@@ -3,6 +3,9 @@ package spil;
 /**
  * @author Daniel Larsen (s151641)
  * @author Daniel Anusic (s155005)
+ * @author Peter El-Habr (s165202)
+ * @author Loui Southwick (s161788)
+ * @author Nemanja Mitrovic (s165231)
  * @version 1.0
  */
 
